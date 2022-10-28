140cc7a89570e2da0fd44ac35b5d976c1867fa7c
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-10-28 17:01:52 +0200

    Adding Fonts to the app ...

--- ecommerce-crown-v2/src/App.js
+++ ecommerce-crown-v2/src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 
-// Styles
-import './categories.styles.scss'
-import CategoryItem from "./components/category-item/category-item.component";
 import Directory from "./components/directory/directory.component";
 
 const App = () => {
